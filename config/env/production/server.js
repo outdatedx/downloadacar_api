59f7d85b2060f54afa9fdffeca1652922fd207b8
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-02-09 21:37:54 +0530

    git push

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env("HEROKU_URL"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  },
 });
