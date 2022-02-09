0273e83f6368920369ca0cdd6547b1c24995305d
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-02-09 21:08:33 +0530

    Recommit heroku

--- config/database.js
+++ config/database.js
@@ -1,11 +1,26 @@
-const path = require('path');
+const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { rejectUnauthorized: false },
+        },
+        options: {
+          ssl: false,
+        },
+      },
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
