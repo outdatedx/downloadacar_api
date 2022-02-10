0353877c3db59c77d6042ffd18bd24fca22554cd
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-02-10 17:46:13 +0530

    Package and DB and Sever Prod update

--- config/database.js
+++ config/database.js
@@ -1,26 +1,15 @@
-const { parse } = require("pg-connection-string");
+const path = require("path");
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false },
-        },
-        options: {
-          ssl: false,
-        },
-      },
+module.exports = ({ env }) => ({
+  connection: {
+    client: "sqlite",
+    connection: {
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
