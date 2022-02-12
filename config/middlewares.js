69c871de9da2e1d61f3a5e74d0e49af04caeb83a
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-02-12 20:49:49 +0530

    middelware update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,28 @@
 module.exports = [
-  "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  // ...
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "downloadacar2.s3.us-west-1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "downloadacar2.s3.us-west-1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  // ...
 ];
