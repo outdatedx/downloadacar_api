6cd2230e212c42f58fa98e0b3e56f47f53846caa
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-02-12 20:44:23 +0530

    plugin js updated

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,14 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "aws-s3",
-    providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: "us-west-1",
-      params: {
-        Bucket: "downloadacar2",
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: "us-west-1",
+        params: {
+          Bucket: "downloadacar2",
+        },
       },
     },
   },
