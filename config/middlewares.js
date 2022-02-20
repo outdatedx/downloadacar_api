d90c7f7f15bef4867dcb20fb0fb1544fae56c50e
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-02-20 11:49:27 +0530

    updated middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -32,5 +32,16 @@ module.exports = [
         },
       },
     },
+    settings: {
+      cors: {
+        enabled: true,
+        headers: "*",
+        origin: [
+          "http://localhost",
+          "https://downloadacar2.s3.us-west-1.amazonaws.com",
+          "https://www.downloadacar.xyz/",
+        ],
+      },
+    },
   },
 ];
