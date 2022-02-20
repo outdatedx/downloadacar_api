d34b4b0be4efb7a8175669d9a8c4f473d93be347
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-02-20 11:43:58 +0530

    default middle ware

--- config/middlewares.js
+++ config/middlewares.js
@@ -32,15 +32,5 @@ module.exports = [
         },
       },
     },
-    settings: {
-      cors: {
-        enabled: true,
-        headers: "*",
-        origin: [
-          "http://localhost",
-          "https://downloadacar2.s3.us-west-1.amazonaws.com",
-        ],
-      },
-    },
   },
 ];
