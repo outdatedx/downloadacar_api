7c3e08507578f362231bac8333d6e0c6b6793f5a
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-02-20 11:40:44 +0530

    middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,34 +1,46 @@
-module.exports = {
-  name: "strapi::security",
-  config: {
-    contentSecurityPolicy: {
-      useDefaults: true,
-      directives: {
-        "connect-src": ["'self'", "https:"],
-        "img-src": [
-          "'self'",
-          "data:",
-          "blob:",
-          "downloadacar2.s3.us-west-1.amazonaws.com",
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "downloadacar2.s3.us-west-1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "downloadacar2.s3.us-west-1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+    settings: {
+      cors: {
+        enabled: true,
+        headers: "*",
+        origin: [
+          "http://localhost",
+          "https://downloadacar2.s3.us-west-1.amazonaws.com",
         ],
-        "media-src": [
-          "'self'",
-          "data:",
-          "blob:",
-          "downloadacar2.s3.us-west-1.amazonaws.com",
-        ],
-        upgradeInsecureRequests: null,
       },
     },
   },
-  settings: {
-    cors: {
-      enabled: true,
-      headers: "*",
-      origin: [
-        "http://localhost",
-        "https://downloadacar2.s3.us-west-1.amazonaws.com",
-      ],
-    },
-  },
-};
+];
