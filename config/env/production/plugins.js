5aa71702e17f3bb5c87a72479cf199d250b80105
// Outdated Designs <OutdatedDesigns@users.noreply.github.com>
2022-12-08 11:53:13 +0530

    U: Vercel

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        accessKeyId: env("DAC_AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("DAC_AWS_ACCESS_SECRET"),
         region: "us-west-1",
         params: {
           Bucket: "downloadacar2",
